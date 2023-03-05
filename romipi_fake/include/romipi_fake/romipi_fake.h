a8cef3e8cf587e4a1960e9159d0eec0ec1aaf69a
Codster130 <cjr130norman@hotmail.com>
2023-03-05 01:30:23 -0500

    more romipi_msgs

--- romipi_fake/include/romipi_fake/romipi_fake.h
+++ romipi_fake/include/romipi_fake/romipi_fake.h
@@ -32,7 +32,7 @@
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 
-#include <turtlebot3_msgs/SensorState.h>
+#include <romipi_msgs/SensorState.h>
 
 #include "romipi_fake.h"
 
